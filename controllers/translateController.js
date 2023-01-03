e9627b4ca55c22e4d0f76de54d77010e92230b55
KulshresthJangid <71918448+KulshresthJangid@users.noreply.github.com>
2023-01-03 12:23:43 +0530

    language code detection and support detection added

--- controllers/translateController.js
+++ controllers/translateController.js
@@ -1,12 +1,27 @@
-const { smartPreCaching, handleTranslationRequest } = require('../helpers/helper')
+const {
+  smartPreCaching,
+  handleTranslationRequest,
+} = require("../helpers/helper");
+const { langs, isSupported, getCode } = require("../helpers/languages");
+const {
+  smartPreCaching,
+  handleTranslationRequest,
+} = require("../helpers/helper");
 
 exports.translate = async (req, res) => {
-    const { text, source, target } = req.body;
-
+  const { text, source, target } = req.body;
+  if (isSupported(source) && isSupported(target)) {
+    source = getCode(source)
+    target = getCode(target)
     const translation = await handleTranslationRequest(text, source, target);
     // Trigger smart pre-caching
     smartPreCaching(text, source, target);
     // send the repsponse
     res.send(translation);
     res.end();
-}
+  } else {
+    res.status(400).send({
+        message: `Sorry this version of API don't support target language ${target} and source language ${source}`
+    })
+  }
+};
